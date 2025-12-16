21e8e8b15fce3ee2e02fbcb443b2a8c5b0871029
Christopher868 <Christopherhicks868@gmail.com>
2025-12-15 19:34:04 -0500

    Handled protecting data from chrome bfcache.

--- website/static/js/view-account.js
+++ website/static/js/view-account.js
@@ -2,18 +2,39 @@ import { decryptSecret } from "./utilities/deriveKey.js";
 const password = document.querySelector('#password');
 const savedIv = document.querySelector('#iv').textContent;
 const usernameOrEmail = document.querySelector('#username-or-email')
+const header = document.querySelector('#acc-info-header')
+
+window.addEventListener('pageshow', (e)=> {
+    if(e.persisted){
+        header.innerHTML = "Refresh page"+ "<br>" +"to view info";
+        usernameOrEmail.textContent = "null";
+        password.textContent = "null"
+    } else {
+        decryptPassword() 
+    }
+    
+});
+
 
 // Calls decrypt function to decrypt encrypted password to be displayed
 async function decryptPassword(){
-    const masterPassword = prompt('Enter master password to decrypt saved account information.')
-    const decryptedUsernameOrEmail = await decryptSecret(masterPassword, usernameOrEmail.textContent, savedIv)
-    const decryptedPassword = await decryptSecret(masterPassword, password.textContent, savedIv)
-    if(decryptPassword === null || decryptedUsernameOrEmail === null){
-        alert("Incorrect master key! Refresh page and try again.")
+    var masterPassword = prompt('Enter master password to decrypt saved account information.')
+    if(masterPassword === null){
+        alert('No password entered please refesh webpage and try again!')
     } else {
-        usernameOrEmail.textContent = decryptedUsernameOrEmail
-        password.textContent = decryptedPassword
+        var decryptedUsernameOrEmail = await decryptSecret(masterPassword, usernameOrEmail.textContent, savedIv)
+        var decryptedPassword = await decryptSecret(masterPassword, password.textContent, savedIv)
+        if(decryptPassword === null || decryptedUsernameOrEmail === null){
+            alert("Incorrect master key! Refresh page and try again.")
+        } else {
+            usernameOrEmail.textContent = decryptedUsernameOrEmail
+            password.textContent = decryptedPassword
+            decryptedUsernameOrEmail = null;
+            decryptedPassword = null;
+            masterPassword = null;
+        }
     }
+    
 }
 
-decryptPassword()
+
